97e0a2c25978c1c52867c537a7cb0f67c65fd74a
Wieczor1 <tdw146@gmail.com>
2021-01-15 02:44:02 +0100

    Add authentication. fix CORS issue

--- src/main/java/com/manager/appmanager/configuration/security/WebSecurityConfig.java
+++ src/main/java/com/manager/appmanager/configuration/security/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.manager.appmanager.configuration.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import com.manager.appmanager.repository.UserRepository;
 import com.manager.appmanager.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
@@ -14,11 +16,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -35,9 +46,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable();
+        http.cors();
             http.csrf().disable() //We don't need CSRF for this example
                 .authorizeRequests().antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()// all request requires a logged in user
@@ -47,8 +62,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/login") //the URL on which the clients should post the login information
                 .usernameParameter("login") //the username parameter in the queryString, default is 'username'
                 .passwordParameter("password") //the password parameter in the queryString, default is 'password'
-                .successHandler(this::loginSuccessHandler)
-                .failureHandler(this::loginFailureHandler)
+                .successHandler(successHandler())
+                .failureHandler(failureHandler())
 
                 .and()
                 .logout()
@@ -71,31 +86,49 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 
-//    @Bean
-//    public DaoAuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider authProvider
-//                = new DaoAuthenticationProvider();
-//        authProvider.setUserDetailsService(customUserDetailsService);
-//        return authProvider;
+//    private void loginSuccessHandler(
+//            HttpServletRequest request,
+//            HttpServletResponse response,
+//            Authentication authentication) throws IOException {
+//        System.out.println("udalo sie");
+//        response.setStatus(HttpStatus.OK.value());
+//        objectMapper.writeValue(response.getWriter(), "Yayy you logged in!");
+//    }
+//
+//    private void loginFailureHandler(
+//            HttpServletRequest request,
+//            HttpServletResponse response,
+//            AuthenticationException e) throws IOException {
+//
+//        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//        response.setContentType("application/json");
+//        System.out.println("wyjebalem sie ");
+//        objectMapper.writeValue(response.getWriter(), "{\"xd\": \"xd\"}");
 //    }
 
-    private void loginSuccessHandler(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Authentication authentication) throws IOException {
-
-        response.setStatus(HttpStatus.OK.value());
-        objectMapper.writeValue(response.getWriter(), "Yayy you logged in!");
+    private AuthenticationSuccessHandler successHandler() {
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                                                HttpServletResponse httpServletResponse, Authentication authentication)
+                    throws IOException, ServletException {
+                httpServletResponse.getWriter().append(objectMapper.writeValueAsString(authentication.getAuthorities()));
+                httpServletResponse.setStatus(200);
+          }
+        };
     }
 
-    private void loginFailureHandler(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            AuthenticationException e) throws IOException {
+    private AuthenticationFailureHandler failureHandler() {
+        return new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest httpServletRequest,
+                                                HttpServletResponse httpServletResponse, AuthenticationException e)
+                    throws IOException, ServletException {
 
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        objectMapper.writeValue(response.getWriter(), "Nopity nop!");
-    }
+                httpServletResponse.getWriter().append("Authentication failure");
+                httpServletResponse.setStatus(401);
+            }
+        };}
 
     private void logoutSuccessHandler(
             HttpServletRequest request,
@@ -105,4 +138,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         response.setStatus(HttpStatus.OK.value());
         objectMapper.writeValue(response.getWriter(), "Bye!");
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedOrigins("http://localhost:4200").allowCredentials(true)
+                        .allowedHeaders("*").exposedHeaders("Set-Cookie");
+            }
+        };
+    }
 }
+
